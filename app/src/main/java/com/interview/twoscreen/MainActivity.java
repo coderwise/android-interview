d2ebda019ddaf55861de6f9984ee76cfdb78902c
Andry Korolyuk <andry@coderwise.com>
2019-04-29 17:36:31 -0400

    - fixed certificate error for debug proxy - added test network call

--- app/src/main/java/com/interview/twoscreen/MainActivity.java
+++ app/src/main/java/com/interview/twoscreen/MainActivity.java
@@ -3,6 +3,17 @@ package com.interview.twoscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.interview.twoscreen.apis.User;
+import com.interview.twoscreen.apis.UserResponse;
+import com.interview.twoscreen.apis.UserService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testLoadUsers();
+    }
+
+    private void testLoadUsers() {
+        UserService service = UserService.Creator.create();
+
+        service.getUsers("50").enqueue(new Callback<UserResponse>() {
+            @Override
+            public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
+                List<User> users = response.body().results;
+
+                for (User user : users) {
+                    Log.i("test", user.email);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserResponse> call, Throwable t) {
+                Log.e("test", "error!");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/interview/twoscreen/apis/User.java
@@ -0,0 +1,8 @@
+package com.interview.twoscreen.apis;
+
+public class User {
+    public String gender;
+    public String email;
+    public String phone;
+    public String cell;
+}
